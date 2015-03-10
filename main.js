0691d2b362b3f1dd8f04a6874bbdf2a4cc9465d1
JC Brand <jc@opkode.com>
2015-03-10 16:51:00 +0100

    Add raptor deps

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ require.config({
     baseUrl: "src",
     paths: {
         "jquery":                       "bower_components/jquery/dist/jquery",
-        "jquery.browser":               "bower_components/jquery.browser/dist/jquery.browser",
+        "jquery-hotkeys":               "bower_components/jquery.hotkeys/jquery.hotkeys",
+        "jqueryui":                     "bower_components/jqueryui/jquery-ui",
+        "rangy":                        "bower_components/rangy/rangy-core",
+        "rangy-serializer":             "bower_components/rangy/rangy-serializer",
+        "rangy-cssclassapplier":        "bower_components/rangy/rangy-cssclassapplier",
+        "rangy-selectionsaverestore":   "bower_components/rangy/rangy-selectionsaverestore",
+        "raptor":                       "../3rdparty/raptor",
         "logging":                      "bower_components/logging/src/logging",
         "pat-base":                     "bower_components/patternslib/src/core/base",
         "pat-compat":                   "bower_components/patternslib/src/core/compat",
@@ -15,7 +21,21 @@ require.config({
         "underscore":                   "bower_components/underscore/underscore",
     },
     "shim": {
-        "logging": { "exports": "logging" }
+        "logging": { "exports": "logging" },
+        "rangy-serializer": { "deps": ["rangy"] },
+        "rangy-cssclassapplier": { "deps": ["rangy"] },
+        "rangy-selectionsaverestore": { "deps": ["rangy"] },
+        "rangy": { "exports": "rangy" },
+        "raptor": {
+            "deps": [
+                "jquery",
+                "jqueryui",
+                "jquery-hotkeys",
+                "rangy-cssclassapplier",
+                "rangy-selectionsaverestore",
+                "rangy-serializer"
+            ]
+        }
     }
 });
 
