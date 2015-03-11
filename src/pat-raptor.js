286475e1240e6fd635a39c594158b6c78bd8ef9f
JC Brand <jc@opkode.com>
2015-03-11 15:20:57 +0100

    Add new config settings
    * toolbar-type
    * toolbar-external
    * toolbar-loading

--- src/pat-raptor.js
+++ src/pat-raptor.js
@@ -20,7 +20,11 @@
 }(this, function($, _, Base, registry, Parser) {
     'use strict';
     var parser = new Parser('raptor');
-    parser.add_argument('config', {}); // Allows the user to directly configure Raptor via JSON
+    // Allows the user to directly configure Raptor via JSON
+    parser.add_argument('config', {});
+    parser.add_argument('toolbar-type', 'standard', ['standard', 'fixed', 'floating']);
+    parser.add_argument('toolbar-external');
+    parser.add_argument('toolbar-loading', 'auto', ['auto', 'click']);
     parser.add_argument('buttons',
             [ 'alignCenter', 'alignJustify', 'alignLeft', 'alignRight',
               'historyRedo', 'historyUndo', 'hrCreate', 'linkCreate',
@@ -46,44 +50,35 @@
         name: 'raptor',
         trigger: ".pat-raptor",
 
-        init: function patRaptorInit() {  
-            this.options = parser.parse(this.$el);
-            /*
-            var cfg = {
-                layouts: {
-                    toolbar: {
-                        uiOrder: [
-                            ['textBold', 'textItalic', 'textUnderline', 'textStrike',
-                              'listOrdered', 'listUnordered', 'linkCreate', 'linkRemove',
-                              'alignLeft', 'alignCenter', 'alignJustify', 'alignRight',
-                              'hrCreate', 'tableCreate', 'historyUndo', 'historyRedo'
-                            ]
-                        ]
-                    },
-                    hoverPanel: { uiOrder: [ ['clickButtonToEdit'] ] },
-                    elementHoverPanel: {
-                        elements: 'img',
-                        uiOrder: [
-                            ['imageResize', 'imageSwap', 'close']
-                        ]
-                    },
-                },
-                // Load the toolbar by default
-                autoEnable: true,
-                // Make sure the toolbar is docked
-                plugins: {
-                    dock: {
-                        docked: true,
-                        dockToElement: true
-                    }
-                }
+        init: function patRaptorInit() {
+            var config = {
+                layouts: { toolbar: {} },
+                plugins: { dock: {} }
             };
-            */
-            $.extend(true, config, this.options.config);
-            if (this.options.buttons instanceof Array) {
-                config.layouts.toolbar.uiOrder = this.options.buttons;
+            this.options = parser.parse(this.$el);
+            config.autoEnable = (this.options.toolbar.loading === "auto") ? true : false;
+
+            switch (this.options.toolbar.type) {
+                case "floating":
+                    config.plugins.dock.dockToElement = false;
+                    config.plugins.dock.docked = false;
+                    break;
+                case "fixed":
+                    config.plugins.dock.dockToElement = false;
+                    config.plugins.dock.docked = true;
+                    break;
+                case "standard":
+                    config.plugins.dock.dockToElement = true;
+                    config.plugins.dock.docked = true;
+                    break;
             }
-            this.$el.raptor(cfg);
+            config.plugins.dock.dockTo = this.options.toolbar.external;
+
+            $.extend(true, config, this.options.config);
+            if (this.options.buttons instanceof Array && this.options.buttons.length) {
+                config.layouts.toolbar.uiOrder = [this.options.buttons];
+            }
+            this.$el.raptor(config);
         }
     });
 }));
