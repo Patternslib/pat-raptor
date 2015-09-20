7b57dba73614302131c9fe9346026f6d0e6c86cf
Rok Garbas <rok@garbas.si>
2015-09-20 15:16:58 +0200

    loading modal html from url with pat-inject

--- src/pat-raptor-image.js
+++ src/pat-raptor-image.js
@@ -10,105 +10,46 @@ define([
     var ImageModal = new Raptor.Button({
       name: 'pat-raptor-image',
       action: function() {
-        this.raptor.pause();
-        this.$modal = $(
-          '<div class="pat-modal">' +
-          ' <h3>Upload or Select Image<h3>' +
-          ' <div>' +
+        var self = this;
 
-          '  <div class="pat-upload" data-pat-upload="url: https://example.org/upload; label: Drop files here to upload or click to browse.; trigger: button" />' +
+        self.raptor.pause();
 
-          '  <form action="/" class="wizard-box " >' +
-          '   <div class="panel-body">' +
-          '    <fieldset class="filter-bar">' +
-          '      <select name="source">' +
-          '        <option>This workspace</option>' +
-          '        <optgroup label="Image bank">' +
-          '          <option>Folder so and so</option>' +
-          '          <option>Folder so and so</option>' +
-          '          <option>Folder so and so</option>' +
-          '        </optgroup>' +
-          '      </select>' +
-          '    </fieldset>' +
+        self.modal = $('#pat-raptor-modal');
+        if (self.modal.length !== 0) {
+          self.modal.remove();
+        }
 
-          '    <fieldset class="pat-checklist radio image-selector">' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/corporate.png" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/klm-tulips.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/lufthansa-winter.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/new-delhi-skyline.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/cms/leitsätze.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/vision-to-product.png" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/book-and-fly.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/budget-proposal.png" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/cms/seelsorgerat.jpeg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/cms/walk.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '      <label class="item">' +
-          '        <input type="radio" name="image" />' +
-          '        <img src="/media/avatar-alexander-pilz.jpg" alt="Description of image." title="Title of image — Description of image" />' +
-          '      </label>' +
-          '    </fieldset>' +
-          '   </div>' +
-          '   <div class="buttons panel-footer">' +
-          '    <button type="submit" class="close-panel icon-ok-circle">' +
-          '     Insert selected image' +
-          '    </button>' +
-          '    <button type="button" class="close-panel icon-cancel-circle">' +
-          '     Cancel' +
-          '    </button>' +
-          '   </div>' +
-          '  </form>' +
+        self.modal = $('<div class="pat-modal large" id="pat-raptor-image"><div/></div>').hide().appendTo('body');
 
-          ' </div>' +
-          '</div>'
-        ).appendTo('body');
+        if (!self.button.hasClass('pat-inject')) {
+            self.button
+              .addClass('pat-inject')
+              .attr('data-pat-inject',
+                      // TODO: url needs to become configurable via pat-raptor
+                      'url:/feedback/panel-image-picker.html;' +
+                      'source:#content;' +
+                      'hooks:raptor;' +
+                      'target:#pat-raptor-image');
 
-        registry.scan(this.$modal);
+            registry.scan(self.button);
 
-        var raptor = this.raptor;
-        $('.pat-upload', this.$modal)
-            .data('pattern-upload')
-            .dropzone.on('complete', function(file) {
-                // TODO: act on completion of uploaded image
-                $(this.element).parents('.pat-modal').data('pattern-modal').destroy()
-            });
+        }
 
-        $('.buttons button').first()  // insert button
-            .on('click', function(e) {
+        self.modal.on('pat-inject-content-loaded-final', function(e) {
+            registry.scan(self.modal);
+
+            self.modal.show();
+            self.modal.trigger('resize.pat-modal-position');
+
+            // insert button
+            $('.buttons button[type=submit]', self.modal).on('click', function(e) {
               e.preventDefault();
               e.stopPropagation();
 
-              raptor.actionApply(function() {
+              self.raptor.actionApply(function() {
                   Raptor.selectionReplace(
+                    // TODO: probably we dont want a close of this image
+                    // element to be inserted
                     $(this).parents('form').find('.checked > img').first().clone()[0]
                   )
               }.bind(this));
@@ -116,12 +57,17 @@ define([
               $(this).parents('.pat-modal').data('pattern-modal').destroy()
             })
 
-        $('.buttons button').last()  // cancel button
-            .on('click', function(e) {
-              e.preventDefault();
-              e.stopPropagation();
-              $(this).parents('.pat-modal').data('pattern-modal').destroy()
-            })
+        });
+
+        self.button.trigger('click.pat-inject')
+
+        // TODO: integrate pat-upload at some point
+        //$('.pat-upload', this.$modal)
+        //    .data('pattern-upload')
+        //    .dropzone.on('complete', function(file) {
+        //        // TODO: act on completion of uploaded image
+        //        $(this.element).parents('.pat-modal').data('pattern-modal').destroy()
+        //    });
       }
     });
 
